0549ad9b25f48aa53e56021d69c42c0764fa9da6
SAT <novikov.andrey.v@hotmail.com>
2020-05-24 13:01:08 +0300

    HelloAppDetails

---
+++ Basic_C_Sharp/HelloAppDetails/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloAppDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Tom";
+            int Age = 20;
+            bool isEmploee = true;
+            double weight = 78.65;
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isEmploee);
+            Console.WriteLine(weight);
+
+        }
+    }
+}
