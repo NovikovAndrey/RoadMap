682ddf51a419e944b041effdce09777a73f96db9
SAT <novikov.andrey.v@hotmail.com>
2020-05-24 12:51:36 +0300

    Add Project

---
+++ Basic_C_Sharp/HelloAppComments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloAppComments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Name = "Tom"; //определение и инициализация
+            Console.WriteLine(Name);//вывод
+            Name = "Bob"; //изменение
+            Console.WriteLine(Name); //вывод
+            Console.ReadKey();
+        }
+    }
+}
