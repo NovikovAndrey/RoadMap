8950ae93cc8b3dea9bff26fb872e2c3245bcb499
SAT <novikov.andrey.v@hotmail.com>
2020-05-24 14:25:51 +0300

    Add Exercises Project

---
+++ Basic_C_Sharp/Exercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercises1
+            Console.WriteLine("Number 1");
+            var Num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Number 2");
+            var Num2 = Convert.ToDouble(Console.ReadLine());
+            if (Num1==Num2)
+            {
+                Console.WriteLine("два числа =");
+            }
+            else 
+                if (Num1<Num2)
+                {
+                    Console.WriteLine("первое число < второго");
+                }
+                else
+                {
+                    Console.WriteLine("первое число > второго");
+                }
+            #endregion
+        }
+    }
+}
