b751e7aecd03cfb47912ee1e751465aa74b7895c
SAT <novikov.andrey.v@hotmail.com>
2020-05-24 14:35:24 +0300

    Add Exercise 4

--- Basic_C_Sharp/Exercises/Program.cs
+++ Basic_C_Sharp/Exercises/Program.cs
@@ -39,15 +39,34 @@ namespace Exercises
             //}
             #endregion
             #region Exercises3
-            Console.WriteLine("Vvedite Number");
+            //Console.WriteLine("Vvedite Number");
+            //var Num = Convert.ToDouble(Console.ReadLine());
+            //if (Num == 5 || Num == 10)
+            //{
+            //    Console.WriteLine("Число == 5 или == 10");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неизвестное число");
+            //}
+            #endregion
+            #region Exercises4
+            Console.WriteLine("Vvedite Amount");
             var Num = Convert.ToDouble(Console.ReadLine());
-            if (Num == 5 || Num == 10)
+            if (Num < 100)
             {
-                Console.WriteLine("Число == 5 или == 10");
+                Console.WriteLine($"New = {Num+Num*0.05}");
             }
             else
             {
-                Console.WriteLine("Неизвестное число");
+                if (Num >= 100 && Num <= 200)
+                {
+                    Console.WriteLine($"New = {Num + Num * 0.07}");
+                }
+                else
+                {
+                    Console.WriteLine($"New = {Num + Num * 0.1}");
+                }
             }
             #endregion
         }
