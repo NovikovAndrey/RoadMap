8e61d5aeab525f1553cd4ab5fff2761b6b6759c7
SAT <novikov.andrey.v@hotmail.com>
2020-05-24 14:29:19 +0300

    Add Exercise 2

--- Basic_C_Sharp/Exercises/Program.cs
+++ Basic_C_Sharp/Exercises/Program.cs
@@ -7,23 +7,36 @@ namespace Exercises
         static void Main(string[] args)
         {
             #region Exercises1
-            Console.WriteLine("Number 1");
-            var Num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Number 2");
-            var Num2 = Convert.ToDouble(Console.ReadLine());
-            if (Num1==Num2)
+            //Console.WriteLine("Number 1");
+            //var Num1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Number 2");
+            //var Num2 = Convert.ToDouble(Console.ReadLine());
+            //if (Num1==Num2)
+            //{
+            //    Console.WriteLine("два числа =");
+            //}
+            //else 
+            //    if (Num1<Num2)
+            //    {
+            //        Console.WriteLine("первое число < второго");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("первое число > второго");
+            //    }
+            #endregion
+
+            #region Exercises2
+            Console.WriteLine("Vvedite Number");
+            var Num = Convert.ToDouble(Console.ReadLine());
+            if (Num>5 && Num<10)
             {
-                Console.WriteLine("два числа =");
+                Console.WriteLine("Число > 5 и < 10");
             }
-            else 
-                if (Num1<Num2)
-                {
-                    Console.WriteLine("первое число < второго");
-                }
-                else
-                {
-                    Console.WriteLine("первое число > второго");
-                }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
             #endregion
         }
     }
