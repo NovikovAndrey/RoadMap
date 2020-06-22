89023a50a1217fa0f215d15232838a5d12cf293a
SAT <novikov.andrey.v@hotmail.com>
2020-06-22 20:53:03 +0300

    Finish

--- EFCoreDBFirst/Program.cs
+++ EFCoreDBFirst/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace EFCoreDBFirst
 {
@@ -6,7 +7,17 @@ namespace EFCoreDBFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (helloappdbContext db = new helloappdbContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Users u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
