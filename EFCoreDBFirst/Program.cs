e71ef2ecfc101add92aa3e0377674e27c119a0d9
SAT <novikov.andrey.v@hotmail.com>
2020-06-22 20:43:58 +0300

    Start

---
+++ EFCoreDBFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreDBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
