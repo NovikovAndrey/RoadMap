93c06cb9af55c1545f4b6a3f05ab1b852c0bd241
SAT <novikov.andrey.v@hotmail.com>
2020-06-22 20:54:42 +0300

    Start

---
+++ EFCoreCRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
