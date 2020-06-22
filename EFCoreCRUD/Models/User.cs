7866c566aa504a631ed981f03539a0534ffedec3
SAT <novikov.andrey.v@hotmail.com>
2020-06-22 21:10:47 +0300

    Finish

---
+++ EFCoreCRUD/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCRUD.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
