719255ea88f795eb80223c30444939dedbf2affc
SAT <novikov.andrey.v@hotmail.com>
2020-06-25 11:30:36 +0300

    Add config

---
+++ EFLogging/DBConnect/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using EFLogging.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFLogging.DBConnect
+{
+    class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- EFLogging/Program.cs
+++ EFLogging/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using EFLogging.Models;
+using EFLogging.DBConnect;
 
 namespace EFLogging
 {
@@ -6,7 +12,30 @@ namespace EFLogging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получаем конфигурацию из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создаем конфигурацию
+            var config = builder.Build();
+            // получаем строку подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            var options = optionsBuilder
+                .UseSqlServer(connectionString)
+                .Options;
+
+            using (ApplicationContext db = new ApplicationContext(options))
+            {
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
